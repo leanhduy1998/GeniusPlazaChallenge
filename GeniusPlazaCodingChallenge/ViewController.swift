8a8647f36b55cfb83f94fa53733f51fc220466eb
Duy Le <leanhduy1998@gmail.com>
2019-07-10 13:04:53 -0400

    Initial Commit

---
+++ GeniusPlazaCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeniusPlazaCodingChallenge
+//
+//  Created by Duy Le 2 on 7/10/19.
+//  Copyright © 2019 Duy Le 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
